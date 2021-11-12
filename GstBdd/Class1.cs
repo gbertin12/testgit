81d995da5f7fea6100c93374f71456382c2ba8e3
Bertin-Gael <65028322+Bertin-Gael@users.noreply.github.com>
2021-11-12 09:10:17 +0100

    Création class Client

--- GstBdd/Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace GstBdd
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ GstBdd/Client.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace GstBdd
+{
+    public class Client
+    {
+        private int idClient;
+        private string nomClient;
+    }
+}
